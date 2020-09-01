faa16943262e1bd20b4096793a3e53391e099494
George <georgedumontier@gmail.com>
2020-09-01 08:20:53 -0500

    broken raycaster is still broken

--- Ray.js
+++ Ray.js
@@ -2,65 +2,90 @@ function Ray(x, y, direction) {
   this.x = x
   this.y = y
   this.direction = direction
-  this.findXIntersect = function(x, y, dy, isUp, skip){
-    // use the point slope formula
-    // y − y1 = m(x − x1)
+  this.findXIntersect = function(x, y, dy, isUp){
     let interceptX = {}
-    interceptX.y = (skip * blockSize) + (isUp ? y - dy : y + dy)
+    interceptX.y = (isUp ? y - dy : y + dy)
     let slope = Math.tan(this.direction)
-    slope = parseFloat(slope.toFixed(2))
-    if (slope == 0.00) {
+    slope = parseFloat(slope.toFixed(1))
+    if (Math.abs(slope) == 0.0) {
       interceptX.dist = 999999999
       return interceptX
     }
     interceptX.x = ((interceptX.y - y) / slope) + x
-    interceptX.dist = Math.sqrt(Math.pow(interceptX.x - x, 2) + Math.pow(interceptX.x - x, 2))
+    interceptX.dist = Math.sqrt(Math.pow(interceptX.x - x, 2) + Math.pow(interceptX.y - y, 2))
 
 
     return interceptX
   }
-  this.findYIntersect = function(x, y, dx, isLeft, skip){
+  this.findYIntersect = function(x, y, dx, isLeft){
+
 
     // vertical lines
     let interceptY = {}
-    interceptY.x = (skip * blockSize) + (isLeft ? x - dx : x + dx)
-    let slope = Math.tan(this.direction)
-    if (slope == 1.00 || slope == -1.00) {
+
+    interceptY.x = (isLeft ? x - dx : x + dx)
+    let slope = parseFloat(Math.tan(this.direction).toFixed(2))
+    if (Math.abs(slope) == 1.00) {
       interceptY.dist = 9999999
       return interceptY
     }
     interceptY.y = ((interceptY.x - x) * slope) + y
-    interceptY.dist = Math.sqrt(Math.pow(interceptY.x - x, 2) + Math.pow(interceptY.x - x, 2))
+    interceptY.dist = Math.sqrt(Math.pow(interceptY.x - x, 2) + Math.pow(interceptY.y - y, 2))
     return interceptY
 
   }
-  // this.findIntersect = function(x, y, dx, dy, isUp, isLeft) {
-  //     // should be able to use the point slope formula
-  //     // y − y1 = m(x − x1)
-  //     let interceptX = {}
-  //     interceptX.y = isUp ? y - dy : y + dy
-  //     interceptX.x = ((interceptX.y - y) / Math.tan(this.direction)) + x
-  //     interceptX.dist = Math.sqrt(Math.pow(interceptX.x - x, 2) + Math.pow(interceptX.x - x, 2))
+  this.findIntersect = function(x, y, dx, dy, isUp, isLeft) {
+      // should be able to use the point slope formula
+      // y − y1 = m(x − x1)
+      let interceptX = {}
+      interceptX.y = isUp ? y - dy : y + dy
+      interceptX.x = ((interceptX.y - y) / Math.tan(this.direction)) + x
+      interceptX.dist = Math.sqrt(Math.pow(interceptX.x - x, 2) + Math.pow(interceptX.x - x, 2))
 
-  //     // vertical lines
-  //     let interceptY = {}
-  //     interceptY.x = isLeft ? x - dx : x + dx
-  //     interceptY.y = ((interceptY.x - x) * Math.tan(this.direction)) + y
-  //     interceptY.dist = Math.sqrt(Math.pow(interceptY.x - x, 2) + Math.pow(interceptY.x - x, 2))
+      // vertical lines
+      let interceptY = {}
+      interceptY.x = isLeft ? x - dx : x + dx
+      interceptY.y = ((interceptY.x - x) * Math.tan(this.direction)) + y
+      interceptY.dist = Math.sqrt(Math.pow(interceptY.x - x, 2) + Math.pow(interceptY.x - x, 2))
 
-  //     return interceptX.dist < interceptY.dist ? [interceptX.x, interceptX.y] : [interceptY.x, interceptY.y]
-  //   }
+      return interceptX.dist < interceptY.dist ? [interceptX.x, interceptX.y] : [interceptY.x, interceptY.y]
+    }
   this.hitWallY = function(x, y) {
       let xBlock = Math.floor(x / blockSize) + (player.x > x ? -1 : 0)
       let yBlock = Math.floor(y / blockSize)
-      // console.log(`${xBlock}, ${yBlock}`)
       return map[yBlock * gridSize + xBlock] === 1
   }
   this.hitWallX = function(x, y) {
     let xBlock =  Math.floor(x / blockSize)
     let yBlock = Math.floor(y / blockSize) + (player.y > y ? -1 : 0)
-    // console.log(`${xBlock}, ${yBlock}`)
     return map[yBlock * gridSize + xBlock] === 1
+  }
+  this.getNextXIntersect = function(lastXIntersect, xStep, isLeft, isUp){
+
+    let newXIntercept = {}
+    if(parseFloat(this.direction.toFixed(2)) == parseFloat(Math.PI.toFixed(2)) || parseFloat(this.direction.toFixed(2)) == parseFloat((2 * Math.PI).toFixed(2)) || parseFloat(Math.abs(this.direction).toFixed(2)) == 0.00) {
+      newXIntercept.dist = 99999999
+      return newXIntercept
+    }
+    newXIntercept.x = lastXIntersect.x + xStep
+
+    newXIntercept.y = lastXIntersect.y + blockSize
+    newXIntercept.dist = Math.sqrt(Math.pow(newXIntercept.x - lastXIntersect.x, 2) + Math.pow(newXIntercept.y - lastXIntersect.y, 2))
+    return newXIntercept
+
+  }
+  this.getNextYIntersect = function(lastYIntersect, yStep, isLeft, isUp){
+
+    let newYIntercept = {}
+    if(parseFloat(this.direction.toFixed(1)) == parseFloat((Math.PI / 2).toFixed(1)) || parseFloat(this.direction.toFixed(2)) == parseFloat((3 * Math.PI /2).toFixed(2))) {
+      newYIntercept.dist = 99999999
+      return newYIntercept
+    }
+    newYIntercept.x = lastYIntersect.x + blockSize
+    newYIntercept.y = lastYIntersect.y + yStep
+    newYIntercept.dist = Math.sqrt(Math.pow(newYIntercept.x - lastYIntersect.x, 2) + Math.pow(newYIntercept.y - lastYIntersect.y, 2))
+    return newYIntercept
+    // isUp ? [yIntercect.x + xStep, yIntersect.y + blockSize] : [yIntersect.x + xStep, yIntersect.y + blockSize]
   }
   this.collide = function () {
 
@@ -80,46 +105,109 @@ function Ray(x, y, direction) {
     let isUp = this.direction > (Math.PI) // looking right
     let dx = isLeft ? this.x - currColumn * blockSize : (currColumn + 1) * blockSize - this.x // horizontal distance to intersection
     let dy = isUp ? this.y - currRow * blockSize : (currRow + 1) * blockSize - this.y // vertical distance to intersection
-    // let xInterceptDist = Math.abs((dy / (Math.cos(this.direction)))) // distance to intersection on the x axis
-    // let yInterceptDist = Math.abs((dx / (Math.cos(this.direction)))) // distance to intersection on the y axis
-    // if(isUp){ yInterceptDist = Math.abs((dx/(Math.cos(Math.PI - this.direction))))}
 
-    // let nextInterceptDist = xInterceptDist <= yInterceptDist ? xInterceptDist : yInterceptDist
-
-    // console.log(`direction: ${this.direction} - isLeft:${isLeft}, isUp: ${isUp} -- dx: ${dx}, dy: ${dy}`)
-
-    let xSkip = 0
-    let ySkip = 0
     let xIntersect = this.findXIntersect(this.x, this.y, dy, isUp, 0)
     let yIntersect = this.findYIntersect(this.x, this.y, dx, isLeft, 0)
-    let coords = null
-    while(!coords){
+    // let yStep = isLeft ? Math.tan(-this.direction) * blockSize : Math.tan(this.direction) * blockSize
+    // let xStep = isUp ? Math.tan((Math.PI / 2 * 3 - -this.direction)) * blockSize : Math.tan((Math.PI / 2 * 3 - this.direction)) * blockSize
+    function invertAngle(angle) {
+      return (angle + Math.PI) % (2 * Math.PI);
+    }
+    // let yStep = isUp ? Math.tan(invertAngle(this.direction)) * blockSize : Math.tan(this.direction) * blockSize
+    // let xStep = isLeft ? Math.tan(invertAngle(Math.PI / 2 - this.direction)) *  blockSize : Math.tan(Math.PI / 2 - this.direction) * blockSize
+    let yStep = Math.atan(this.direction) * blockSize
+    let xStep = Math.atan(Math.PI / 2 - this.direction) *  blockSize
+    //if (isLeft) {xStep = Math.tan(this.direction) * blockSize}
 
-      if(xIntersect.dist < yIntersect.dist) {
-        if(this.hitWallX(xIntersect.x, xIntersect.y)){
-          coords = [xIntersect.x, xIntersect.y]
-        } else {
-          ySkip++
-          xIntersect = this.findXIntersect(this.x, this.y, dy, isUp, ySkip)
-        }
+
+    function drawSteps(steps) {
+
+      ctx.beginPath()
+      ctx.moveTo(player.x, player.y)
+      ctx.lineTo(steps.xStep.x[1], steps.xStep.y[1])
+      ctx.closePath()
+      ctx.strokeStyle = 'purple'
+      ctx.stroke()
+
+      ctx.beginPath()
+      ctx.moveTo(player.x, player.y)
+      ctx.lineTo(steps.yStep.x[1], steps.yStep.y[1])
+      ctx.closePath()
+      ctx.strokeStyle = 'blue'
+      ctx.stroke()
+    }
+
+
+    drawSteps({
+      xStep:{
+        x: [this.x, isLeft ? this.x - dx : this.x + dx],
+        y: [this.y, this.y]
+      },
+      yStep:{
+        x: [this.x, this.x],
+        // y: [this.y, this.y + dy]
+        y: [this.y, isUp ? this.y - dy : this.y + dy]
       }
-      else{
-        if(this.hitWallY(yIntersect.x, yIntersect.y)){
-          coords = [yIntersect.x, yIntersect.y]
+    })
+
+
+    let coords = null
+    let i = 0
+    while(!coords){
+      i++
+
+      let nextXIntersect = this.getNextXIntersect(xIntersect, xStep, isLeft, isUp)
+      let nextYIntersect = this.getNextYIntersect(yIntersect, yStep, isLeft, isUp)
+      if(nextXIntersect.dist < nextYIntersect.dist){
+        if(this.hitWallX(nextXIntersect.x, nextXIntersect.y)){
+          coords = [nextXIntersect.x, nextXIntersect.y]
         } else{
-          xSkip++
-          yIntersect = this.findYIntersect(this.x, this.y, dx, isLeft, xSkip)
+          if(i>50) coords = [0,0]
+          xIntersect = nextXIntersect
+        }
+      } else{
+        if(this.hitWallY(nextYIntersect.x, nextYIntersect.y)) {
+          coords = [nextYIntersect.x, nextYIntersect.y]
+        } else{
+          if(i > 50) coords =[0,0]
+          yIntersect = nextYIntersect
         }
       }
     }
-
+    console.log(`direction:${this.direction}, xStep:${xStep} , yStep:${yStep} xIntersect.dist:${xIntersect.dist}, yIntersect.dist: ${yIntersect.dist}`)
     return coords
+    // return coords
+    // return xIntersect.dist < yIntersect.dist ? [xIntersect.x, xIntersect.y] : [yIntersect.x, yIntersect.y]
+  }
+}
+    // let coords = null
+    // while(!coords){
+
+    //   if(xIntersect.dist < yIntersect.dist) {
+    //     if(this.hitWallX(xIntersect.x, xIntersect.y)){
+    //       coords = [xIntersect.x, xIntersect.y]
+    //     } else {
+    //       ySkip++
+    //       xIntersect = this.findXIntersect(this.x, this.y, dy, isUp, ySkip)
+    //     }
+    //   }
+    //   else{
+    //     if(this.hitWallY(yIntersect.x, yIntersect.y)){
+    //       coords = [yIntersect.x, yIntersect.y]
+    //     } else{
+    //       xSkip++
+    //       yIntersect = this.findYIntersect(this.x, this.y, dx, isLeft, xSkip)
+    //     }
+    //   }
+    // }
+
+    // return coords
     // this.findIntersect
     //  if this.hitWall return coords
     // else add a skip and run it again until we find one
 
 
-  }
+
 
 
   /*
@@ -213,4 +301,3 @@ function Ray(x, y, direction) {
   //   let finalCoords = getEndPoints(this.direction, this.x, this.y)
   //   return finalCoords
   // }
-}
--- sizeCanvas.js
+++ sizeCanvas.js
@@ -3,7 +3,10 @@ function sizeCanvas() {
   const canvas = document.getElementById("canvas");
   width = window.innerWidth;
   height = window.innerHeight;
-  blockSize = height > width ? parseInt(width / gridSize) : parseInt(height / gridSize)
+  blockSize = 100
   canvas.width = blockSize * 8;
   canvas.height = blockSize * 8;
+  // blockSize = height > width ? parseInt(width / gridSize) : parseInt(height / gridSize)
+  // canvas.width = blockSize * 8;
+  // canvas.height = blockSize * 8;
 }
